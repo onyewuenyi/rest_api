99145ce62b28218c2035183a9f10ee466614ae2c
BlackDynamite <onyewuenyi@gmail.com>
2021-09-05 21:16:07 -0500

    udpate

--- pkg/util/config/config_test.go
+++ pkg/util/config/config_test.go
@@ -1,10 +1,18 @@
 package config_test
 
 import (
+	"fmt"
 	"os"
 	"testing"
+
+	"github.com/onyewuenyi/rest_api/tree/main/pkg/util/config"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLoad(t *testing.T) {
 	os.Setenv("APP_ENVIRONMENT", "production")
+	cfg, err := config.InitConfig()
+
+	fmt.Println(cfg)
+	assert.Equal(1, cfg.Server)
 }
